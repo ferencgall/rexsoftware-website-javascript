3c9b7376e423cfca10f1c58e41aabc2f4e8c294d
ferencgall <98003306+ferencgall@users.noreply.github.com>
2022-01-19 13:47:29 +1000

    Create region-selector.js

---
+++ region-selector.js
@@ -0,0 +1,54 @@
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.7.1.min.js"></script>
+<script>
+    if (localStorage.getItem("Region") === null) {
+        show_popup();
+    }
+
+    function show_popup() {
+        $("#region-selector-container").css("display", "flex");
+        $("#cookie-banner-bottom").css("display", "none");
+    }
+
+    function hide_popup() {
+        $("#region-selector-container").css("display", "none");
+        $("#cookie-banner-bottom").css("display", "block");
+    }
+
+    $("#au-button").click(function() {
+        localStorage.setItem("Region", "AU");
+        hide_popup();
+    });
+
+    $("#nz-button").click(function() {
+        localStorage.setItem("Region", "NZ");
+        hide_popup();
+    });
+
+    $("#uk-button").click(function() {
+        localStorage.setItem("Region", "UK");
+        hide_popup();
+        location.reload();
+    });
+
+    $("#global-button").click(function() {
+        localStorage.setItem("Region", "Global");
+        hide_popup();
+        location.reload();
+    });
+
+    $("#au-button-mobile").click(function() {
+        localStorage.setItem("Region", "AU");
+        hide_popup();
+    });
+
+    $("#nz-button-mobile").click(function() {
+        localStorage.setItem("Region", "NZ");
+        hide_popup();
+    });
+
+    $("#uk-button-mobile").click(function() {
+        localStorage.setItem("Region", "UK");
+        hide_popup();
+        location.reload();
+    });
+</script>
